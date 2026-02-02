40c3121743c66955ea22261afe64fef274cf966e
Mathias Lyngbye <mathias@allesen.dk>
2026-02-02 19:24:32 +0100

    Update install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 mkdir -p build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/.local
-cmake --build . --parallel
-cmake --install .
-
-systemctl --user daemon-reload
-systemctl --user enable --now gsus.service
+cmake ..
+sudo cmake --install .
